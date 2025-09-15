4fdf4c0786424fad10cadfa6ce46a1d57b4db9bb
kimhaeun123 <128309139+KimHaeun123@users.noreply.github.com>
2025-09-15 15:06:11 +0900

    first commit

---
+++ src/main/java/com/emolog/emolog_backend/EmologBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emolog.emolog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmologBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmologBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emolog/emolog_backend/EmologBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emolog.emolog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmologBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
